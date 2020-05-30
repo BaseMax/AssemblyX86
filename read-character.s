f64d72489aaaa41df14c96ba31ed55f30538e7aa
Max Base <MaxBaseCode@Gmail.Com>
2020-05-31 01:06:47 +0430

    Update read-character.s

--- read-character.s
+++ read-character.s
@@ -3,15 +3,15 @@
 ; **********************************************
 global _start
 section .data
-  inChar db 0
+	inChar db 0
 section .text
 _start:
-  mov rax, 0 ; SYS_read
-  mov rdi, 0 ; STDIN
-  mov rsi, inChar
-  mov rdx, 3 ; read count characters
-  syscall
+	mov rax, 0 ; SYS_read
+	mov rdi, 0 ; STDIN
+	mov rsi, inChar
+	mov rdx, 3 ; read count characters
+	syscall
 done:
-  mov rax, 60 ; SYS_exit
-  mov rdi, 0 ; success
-  syscall
+	mov rax, 60 ; SYS_exit
+	mov rdi, 0 ; success
+	syscall
