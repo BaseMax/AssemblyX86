cf661689757406e05a6a5c605b5bf7096584a68d
Max Base <MaxBaseCode@Gmail.Com>
2020-05-31 01:06:34 +0430

    Update hello-world.s

--- hello-world.s
+++ hello-world.s
@@ -4,15 +4,15 @@
 global  _start
 section .data
 section  .text
-  msg  db  'Hello, world!',0xa
-  len  equ $ - msg
+	msg  db  'Hello, world!',0xa
+	len  equ $ - msg
 _start:
-  ; write()
-  mov  edx, len
-  mov  ecx, msg
-  mov  ebx, 1
-  mov  eax, 4
-  int  0x80
-  ; exit()
-  mov  eax, 1
-  int  0x80
+	; write()
+	mov  edx, len
+	mov  ecx, msg
+	mov  ebx, 1
+	mov  eax, 4
+	int  0x80
+	; exit()
+	mov  eax, 1
+	int  0x80
